fe0ffe85f713495bdae95f30a3beae2e5e01a00b
RandomCivil <2357243383@qq.com>
2016-01-26 23:07:51 +0800

    init

---
+++ js/components/index.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <span>hello world</span>,
+  document.body.appendChild(document.createElement('div'))
+);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: ['webpack/hot/dev-server','webpack-dev-server/client?http://localhost:8080','./js/components/index'],
+  externals: [{
+    'react-dom':'ReactDOM',
+    'react':'React'
+  }],
+  output: {
+    path:path.resolve(__dirname,'js/components'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders:[
+    {
+      test: /\.js[x]?$/,
+      include: path.resolve(__dirname, 'js/components'),
+      loader: 'babel',
+      query: {
+        presets: ['es2015','react']
+      }
+    },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias:[
+    {'react-dom':'./libs/react-dom.js','react':'./libs/react.js'}
+    ]
+  },
+  plugins: [
+  new webpack.HotModuleReplacementPlugin()
+  ]
+};
